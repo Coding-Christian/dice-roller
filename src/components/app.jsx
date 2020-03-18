08185526349446fe8699f83e2e55d604c5964545
Coding-Christian <christianhaney1996@gmail.com>
2020-03-18 12:45:52 -0700

    dice can now be rerolled

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,26 +2,30 @@ import React, { useState } from 'react';
 import Dice from './dice';
 
 function App(props) {
-  let [numDice, setNumDice] = useState(0);
-  let [diceVals, setDiceVals] = useState([]);
+  const [diceVals, setDiceVals] = useState([]);
   const options = [1, 2, 3, 4, 5, 6];
   function changeNumDice(change) {
     if (change > 0) {
       const randVal = options[Math.floor(Math.random() * options.length)];
       setDiceVals(diceVals => [...diceVals, randVal]);
-      setNumDice(num => num + change);
-    } else if (numDice > 0) {
+    } else if (diceVals.length) {
       setDiceVals(diceVals => diceVals.slice(0, diceVals.length + change));
-      setNumDice(num => num + change);
     }
+  }
+  function rollDice() {
+    const newDiceVals = [];
+    for (let i = 0; i < diceVals.length; i++) {
+      newDiceVals.push(options[Math.floor(Math.random() * options.length)]);
+    }
+    setDiceVals(newDiceVals);
   }
   return (<>
     <h1>Total: {diceVals.reduce((acc, curr) => acc + curr, 0)}</h1>
     <div className='dice-controls'>
       <button onClick={() => changeNumDice(-1)}>-</button>
-      {numDice}
+      {diceVals.length}
       <button onClick={() => changeNumDice(1)}>+</button>
-      <button>Roll</button>
+      <button onClick={rollDice}>Roll</button>
     </div>
     <div className='dice-area'>
       {diceVals.map((value, index) => (<Dice key={index} value={value} />))}
