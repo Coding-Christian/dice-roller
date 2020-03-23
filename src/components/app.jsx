d79001d3760b32f51d4271815b56ced0d99121da
Coding-Christian <christianhaney1996@gmail.com>
2020-03-23 10:50:42 -0700

    fixed spacing of DOM elements

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,14 +5,13 @@ function App(props) {
   const [numTrays, setNumTrays] = useState(1);
   const trays = [];
   for (let i = 0; i < numTrays; i++) {
-    trays.push(<DiceTray key={i} numSides={6}/>);
+    trays.push(<DiceTray key={i}/>);
   }
   return (
     <div className='app'>
       <div className="app-controls">
-        <button onClick={() => setNumTrays(num => num - 1)}>-</button>
-          Trays
-        <button onClick={() => setNumTrays(num => num + 1)}>+</button>
+        <button onClick={() => setNumTrays(num => num - 1)}> Remove Tray</button>
+        <button onClick={() => setNumTrays(num => num + 1)}>Add Tray</button>
       </div>
       <hr/>
       <div className="app-dice-trays">
